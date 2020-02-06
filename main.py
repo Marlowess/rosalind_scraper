f5429058170a82e0209b81ce526c06ab0b2274a6
Stefano <brll.stefano@gmail.com>
2020-02-06 16:59:45 +0000

    Added prompt

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
 from request_handler import Request_handler
 
 if __name__ == '__main__':
-  handler = Request_handler()
-  # handler.print_chapters()
-  handler.read_chapter(3)
+  handler = Request_handler() 
+
+  while True:
+    print('What chapter do you wanna read? -1 to exit, -2 to see the summary')
+    chap = input()
+    if chap == '-1':
+      print('Got it, see you!')
+      exit(0)
+    elif chap == '-2':
+      handler.print_chapters()
+      print('***************************************************************')
+      continue 
+    handler.read_chapter(int(chap))
+    print('***************************************************************')
+    
